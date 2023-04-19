c900b37462952406344965db7a5a17c0e388f994
Nathan <nathan@naykel.com.au>
2023-04-19 13:03:14 +1000

    add home and login scafolding and basic routing

---
+++ nk_tasks.md
@@ -0,0 +1,14 @@
+GreenBank RSL
+54 Anzac Ave, Hillcrest QLD 4118
+
+Runcorn Tavern
+124 Gowan Rd, Sunnybank Hills QLD 4109
+
+Souths Sports Club
+174 Mortimer Rd, Acacia Ridge QLD 4110
+
+Rocklea Hotel
+1337 Ipswich Rd, Rocklea QLD 4106
+
+
+Will we need to implement user authentication including forgotten passwords?
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+  { path: '', redirectTo: 'home', pathMatch: 'full', },
+  { path: 'home', loadComponent: () => import('./home/home.page').then((m) => m.HomePage), },
+  { path: 'login', loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage) },
 ];
